3efd8cc92b89f92306739ecaacbb7365f9ecb80c
julio gabriel <juliogabriel78956@gmail.com>
2025-11-24 16:27:53 -0300

    Estudo com JavaScript

---
+++ app.js
@@ -0,0 +1,48 @@
+//  24/11/25  // VARIAVEIS 
+
+// var
+// let
+// const
+
+// TIPOS DE DADOS
+
+// Number
+// String
+// Boolean
+// Null
+// Undefined
+
+// NUmber
+// let numero = 109
+// console.log(numero)
+// console.log(typeof numero)
+
+// String
+// let texto = "Olá meu Amigo, o sonho ira se torna realidade pode ter certeza"
+// let nome = "GAbriel"
+// console.log(texto + " " + nome)
+
+// Boolean
+
+// let verdadeiro = true
+// console.log(typeof verdadeiro)
+
+
+// LISTA SOBRE OBJECTS TREINO
+// const produto = {
+//   nome: "Camisa",
+//   preco: 59.90,
+//   estoque: 12
+// };
+// console.log(" O preço total da camisa e : " + produto.preco)
+
+// Usando o objeto do exercício anterior, mude o valor de estoque para 7
+//   produto.preco = 7
+//   console.log(produto.preco)
+
+// Ainda com o mesmo objeto, adicione a propriedade categoria com o valor "Roupas".
+
+// produto.categoria = "Roupas";
+// console.log(produto.categoria)
+
+
