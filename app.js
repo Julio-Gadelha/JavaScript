263e19670700de1b78f82dcc8c707b57b9733c92
julio gabriel <juliogabriel78956@gmail.com>
2025-12-01 16:54:16 -0300

    JAVASCRIPT

--- app.js
+++ app.js
@@ -78,125 +78,125 @@
 //         id: 5,
 //         modelo: "Fiesta",
 //         ano: 2010,
-//         cor: "Branca"
+// //         cor: "Branca"
+// //     },
+// //     { 
+// //         id: 6,
+// //         modelo: "Kangoo",
+// //         ano: 2015,
+// //         cor: "Prata"
+// //     },
+// //     { 
+// //         id: 7,
+// //         modelo: "Uno",
+// //         ano: 1995,
+// //         cor: "Verde"
+// //     },
+// //     { 
+// //         id: 8,
+// //         modelo: "Saveiro",
+// //         ano: 2012,
+// //         cor: "Preta"
+// //     },
+// //     { 
+// //         id: 9,
+// //         modelo: "Strada",
+// //         ano: 2017,
+// //         cor: "Branca"
+// //     },
+// //     { 
+// //         id: 10,
+// //         modelo: "Siena",
+// //         ano: 2008,
+// //         cor: "Cinza"
+// //     }
+// // ];
+
+
+// //Acesse o modelo do carro que tem o id 3 e exiba uma frase com seu nome.
+// // console.log(`O carro do modelo ${carros[2].modelo}`);
+
+// //Acesse a cor do carro com o modelo "Fiesta" e crie uma frase informando a cor do carro.
+// // console.log(` o Carro ${carros[4].modelo} e da cor ${carros[4].cor}`)
+
+// //Crie uma frase dizendo o ano do carro que tem o id 7
+// //  console.log(` O carro  do modelo  ${carros[6].modelo} e do ano ${carros[6].ano}`)
+
+// //Exiba o modelo e a cor do carro que foi fabricado em 2005.
+// // console.log(` O carro de modelo ${carros[2].modelo} tem a cor ${carros[2].cor} e foi fabricado no ano de ${carros[2].ano}`)
+
+// //Crie uma frase que diga o modelo e o ano do carro com id 10.
+// // console.log(` O carro de modelo ${carros[9].modelo} e do ano ${carros[9].ano}`)
+
+// //Crie uma frase dizendo que o carro com o id 5 é branco e foi fabricado em 2010.
+// // console.log(` O carro de modelo ${carros[4].modelo} foi fabricado no ano de ${carros[4].ano}`)
+
+
+
+
+// // ARRAYS TAREFA PARA TREINAR
+// let filmes = [
+//     { 
+//         id: 1,
+//         titulo: "O Senhor dos Anéis: A Sociedade do Anel",
+//         ano: 2001,
+//         genero: "Fantasia"
+//     },
+//     { 
+//         id: 2,
+//         titulo: "O Poderoso Chefão",
+//         ano: 1972,
+//         genero: "Crime"
+//     },
+//     { 
+//         id: 3,
+//         titulo: "Forrest Gump: O Contador de Histórias",
+//         ano: 1994,
+//         genero: "Drama"
+//     },
+//     { 
+//         id: 4,
+//         titulo: "Star Wars: O Império Contra-Ataca",
+//         ano: 1980,
+//         genero: "Ficção científica"
+//     },
+//     { 
+//         id: 5,
+//         titulo: "A Origem",
+//         ano: 2010,
+//         genero: "Ficção científica"
 //     },
 //     { 
 //         id: 6,
-//         modelo: "Kangoo",
-//         ano: 2015,
-//         cor: "Prata"
+//         titulo: "O Rei Leão",
+//         ano: 1994,
+//         genero: "Animação"
 //     },
 //     { 
 //         id: 7,
-//         modelo: "Uno",
-//         ano: 1995,
-//         cor: "Verde"
+//         titulo: "Titanic",
+//         ano: 1997,
+//         genero: "Romance"
 //     },
 //     { 
 //         id: 8,
-//         modelo: "Saveiro",
-//         ano: 2012,
-//         cor: "Preta"
+//         titulo: "Vingadores: Ultimato",
+//         ano: 2019,
+//         genero: "Ação"
 //     },
 //     { 
 //         id: 9,
-//         modelo: "Strada",
-//         ano: 2017,
-//         cor: "Branca"
+//         titulo: "O Exorcista",
+//         ano: 1973,
+//         genero: "Terror"
 //     },
 //     { 
 //         id: 10,
-//         modelo: "Siena",
-//         ano: 2008,
-//         cor: "Cinza"
-//     }
-// ];
-
-
-//Acesse o modelo do carro que tem o id 3 e exiba uma frase com seu nome.
-// console.log(`O carro do modelo ${carros[2].modelo}`);
-
-//Acesse a cor do carro com o modelo "Fiesta" e crie uma frase informando a cor do carro.
-// console.log(` o Carro ${carros[4].modelo} e da cor ${carros[4].cor}`)
-
-//Crie uma frase dizendo o ano do carro que tem o id 7
-//  console.log(` O carro  do modelo  ${carros[6].modelo} e do ano ${carros[6].ano}`)
-
-//Exiba o modelo e a cor do carro que foi fabricado em 2005.
-// console.log(` O carro de modelo ${carros[2].modelo} tem a cor ${carros[2].cor} e foi fabricado no ano de ${carros[2].ano}`)
-
-//Crie uma frase que diga o modelo e o ano do carro com id 10.
-// console.log(` O carro de modelo ${carros[9].modelo} e do ano ${carros[9].ano}`)
-
-//Crie uma frase dizendo que o carro com o id 5 é branco e foi fabricado em 2010.
-// console.log(` O carro de modelo ${carros[4].modelo} foi fabricado no ano de ${carros[4].ano}`)
-
-
-
-
-// ARRAYS TAREFA PARA TREINAR
-let filmes = [
-    { 
-        id: 1,
-        titulo: "O Senhor dos Anéis: A Sociedade do Anel",
-        ano: 2001,
-        genero: "Fantasia"
-    },
-    { 
-        id: 2,
-        titulo: "O Poderoso Chefão",
-        ano: 1972,
-        genero: "Crime"
-    },
-    { 
-        id: 3,
-        titulo: "Forrest Gump: O Contador de Histórias",
-        ano: 1994,
-        genero: "Drama"
-    },
-    { 
-        id: 4,
-        titulo: "Star Wars: O Império Contra-Ataca",
-        ano: 1980,
-        genero: "Ficção científica"
-    },
-    { 
-        id: 5,
-        titulo: "A Origem",
-        ano: 2010,
-        genero: "Ficção científica"
-    },
-    { 
-        id: 6,
-        titulo: "O Rei Leão",
-        ano: 1994,
-        genero: "Animação"
-    },
-    { 
-        id: 7,
-        titulo: "Titanic",
-        ano: 1997,
-        genero: "Romance"
-    },
-    { 
-        id: 8,
-        titulo: "Vingadores: Ultimato",
-        ano: 2019,
-        genero: "Ação"
-    },
-    { 
-        id: 9,
-        titulo: "O Exorcista",
-        ano: 1973,
-        genero: "Terror"
-    },
-    { 
-        id: 10,
-        titulo: "Jurassic Park",
-        ano: 1993,
-        genero: "Aventura"
-    }	
-]; 
+//         titulo: "Jurassic Park",
+//         ano: 1993,
+//         genero: "Aventura"
+//     }	
+// ]; 
 
 // Acesse o título do filme com o id 4 e exiba uma frase dizendo que o filme é do gênero "Ficção científica".
 // console.log(`O filme ${filmes[3].titulo} é do gênero ${filmes[3].genero}`)
@@ -220,3 +220,34 @@ let filmes = [
 
 // Exiba o título e o gênero do filme que foi lançado em 2001.
 // console.log(` O filme ${filmes[0].titulo} e do genero ${filmes[0].genero} e foi lançado no ano de ${filmes[0].ano}`)
+
+
+
+// QUESTIONS SOBRE IF 01/12/25
+
+// Escreva um código que determine se uma pessoa é idosa, considerando que uma pessoa é idosa quando tem 60 anos ou mais.
+// 
+
+// let idade = 57
+// if (idade > 60) {
+//     console.log("Você e considerado idoso ou idosa")
+// } else if(idade <60) {
+//     console.log("Você não e idoso ou idosa")
+// }
+
+
+
+// Escreva um código para classificar o desempenho de um aluno com base na sua nota. Se a nota for maior ou igual a 7, o aluno foi aprovado; se for menor que 7 e maior ou igual a 5, o aluno está de recuperação; e se for menor que 5, o aluno foi reprovado.
+
+// let nota = 3
+
+// if (nota >= 7) {
+//     console.log("Você foi aprovado")
+// } else if (nota < 7 && nota >= 5) {
+//     console.log("Você está de recuperação, tenha calma vai dar certo")
+// } else if (nota < 5) {
+//     console.log("Você foi reprovado")
+// }
+
+
+
